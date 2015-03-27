73043a2ea4f99c790c155364383dc3d13cc53559
Alice Cecile <alice.i.cecile@gmail.com>
2015-03-27 08:30:05 -0400

    Basic structure for one pass of analysis

---
+++ analysis.R
@@ -0,0 +1,40 @@
+# Data processing and model fitting ####
+
+# Load in script that does the heavy lifting
+source("modelling.R")
+
+# Perform a round of data processing and model fitting on a subset of the data
+model_bathymetry <- function(data_path, threshold){
+    
+    data_set <- read.csv(data_path)
+    
+    crude_residuals <- find_crude_local_residuals(data_set)
+    
+    outlier_list <- detect_outliers(crude_residuals, threshold)
+    
+    cleaned_data_set <- data_set[-outlier_list,]
+    
+    point_weights <- weight_points(cleaned_data_set, crude_residuals)
+    
+    depth_model <- fit_depth_model(cleaned_data_set, point_weights)
+    
+    output <- list(depth_model=depth_model, weights=point_weights, outliers=data_set[outlier_list,])
+    
+    return(output)
+}
+
+# Experiment with different options
+
+# Select and save final model and data
+
+# Prediction ####
+
+# Predict depth at prespecified points
+predict_points <- function(points, model){
+    return(NULL)
+}
+
+# Predict depth data in a grid
+predict_griddded <- function(bounding_region, resolution, model){
+    return(NULL)
+}
---
+++ modelling.R
@@ -0,0 +1,45 @@
+# True outlier detection ####
+find_crude_local_residuals <- function(data_set){
+    # Fit a simple local model to the data
+    
+    # Compute approximate residuals for each data point
+    
+    return(NULL)
+} 
+
+
+detect_outliers <- function(resids, threshold=3){
+    
+    # Standardize the residuals
+    
+    # Flag all points with an absolute standardized residual > threshold
+    
+    return(NULL)
+}
+
+# Weighting remaining points ####
+find_ship_path(tract){
+    return(NULL)
+}
+
+weight_points <- function(data_set, resids){
+    
+    # Find path of ship in each tract
+    
+    # Compute distance of each point to the path
+    # This is the horizontal distance it was measured from
+        
+    # Regress square of residuals vs. the distance to the path
+    
+    # Weight each point according to 1 / predicted error (squared residual) for that distance
+    
+    return(NULL)
+}
+
+# Model fitting ####
+fit_depth_model <- function(data_set, weights=NA){
+    
+    # Fit a smooth depth model, using the determined weights
+    
+    return(NULL)
+}
